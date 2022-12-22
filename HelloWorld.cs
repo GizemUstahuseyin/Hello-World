f94f1cc2a861bd4f4a960cc529fec41e5ff0c21f
Gizem <gizemustahuseyin123@gmail.com>
2022-12-22 22:24:52 +0300

    Add files via upload

---
+++ HelloWorld.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2._8_GirişHelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+    }
+}
